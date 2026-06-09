9aeaa2bb6cec754179669973448f54deb2697416
rawan2610 <rawanhesham465@gmail.com>
2026-06-09 23:33:11 +0300

    Fixed frontend folder - removed backend files from ems-frontend

--- ems-frontend/DTOs/SectorDTOs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EMS.API.DTOs
-{
-    public class SectorResponseDTO
-    {
-        public int SectorID { get; set; }
-        public string SectorName { get; set; } = string.Empty;
-    }
-}
